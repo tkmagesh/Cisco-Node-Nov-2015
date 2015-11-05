4c728c96181c9228b7e5c6c5f8116bd653eda23e
tkmagesh <tkmagesh77@gmail.com>
2015-11-05 12:33:36 +0530

    async sequence - challenge - solution

--- 02-Day/08-AsyncSequence/sequence.js
+++ 02-Day/08-AsyncSequence/sequence.js
@@ -26,34 +26,66 @@ module.exports.runSync = function runSync(){
     f4Sync();
 }
 
-function f1(){
+function f1(next){
     console.log('f1 is invoked');
     setTimeout(function(){
         console.log('f1 is completed');
+        if (typeof next === 'function')
+            next();
     },3000);
 }
-function f2(){
+function f2(next){
     console.log('f2 is invoked');
     setTimeout(function(){
         console.log('f2 is completed');
+        if (typeof next === 'function')
+            next();
     },3000);
 }
-function f3(){
+function f3(next){
     console.log('f3 is invoked');
     setTimeout(function(){
         console.log('f3 is completed');
+        if (typeof next === 'function')
+            next();
     },3000);
 }
-function f4(){
+function f4(next){
     console.log('f4 is invoked');
     setTimeout(function(){
         console.log('f4 is completed');
+        if (typeof next === 'function')
+            next();
+    },3000);
+}
+function f5(next){
+    console.log('f5 is invoked');
+    setTimeout(function(){
+        console.log('f5 is completed');
+        if (typeof next === 'function')
+            next();
     },3000);
 }
 
+var fns = [f1, f2, f3, f4, f5];
+
 module.exports.run = function run(){
-    f1();
-    f2();
-    f3();
-    f4();
+
+    function exec(fns){
+        var first = fns[0],
+            remaining = fns.slice(1),
+            next = function(){
+                exec(remaining);
+            };
+        if (typeof first === 'function')
+            first(next);
+    }
+    exec(fns);
+    /*f1(function(){
+        f2(function(){
+            f3(function(){
+                f4();
+            })
+        })
+    })*/
 }
